f1735c3706a3b3ba708a6fb680f2ec14abed9a78
LyudmilaRomanchenko <nowbeauty31@gmail.com>
2021-09-23 23:03:18 +0300

    Добавляет исхоные элементы

---
+++ src/index.js
@@ -0,0 +1,44 @@
+//  import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+// const a = React.createElement('a', {
+//   href: 'github.com/LyudmilaRomanchenko/goit-react-hw-01-components',
+// },
+//   'Ссылка'
+// )
+// const b = {
+//   s: 'Ссылка fffff',
+// }
+
+//
+
+
+
+
+
+ReactDOM.render( <App />,
+  
+  document.getElementById('root')
+);
+
+
+
+
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import './index.css';
+// import App from './App';
+// import reportWebVitals from './reportWebVitals';
+
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
+
+// // If you want to start measuring performance in your app, pass a function
+// // to log results (for example: reportWebVitals(console.log))
+// // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// reportWebVitals();
