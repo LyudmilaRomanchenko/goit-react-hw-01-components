1c6fc1add64416af3f28f7229c62d0b07852a833
LyudmilaRomanchenko <nowbeauty31@gmail.com>
2021-09-24 19:47:24 +0300

    Добавляет маркер в задачу 3

--- src/components/FriendList/FriendList.js
+++ src/components/FriendList/FriendList.js
@@ -1,11 +1,15 @@
 // import friends from 'путь/к/friends.json';
 
+import s from './FriendList.module.css';
+console.log(s.status);
+
 function FriendList({ friends }) {
   return (
     <ul className="friend-list">
+      {/* const status = friend.isOnline; */}
       {friends.map(friend => (
         <li className="item" key={friend.id}>
-          <span className="status"></span>
+          <span className={friend.isOnline ? s.online : s.offline}></span>
           <img className="avatar" src={friend.avatar} alt="" width="48" />
           <p className="name">{friend.name}</p>
         </li>
